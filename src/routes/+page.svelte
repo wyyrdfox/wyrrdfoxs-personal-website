5e4b94188dd7468da57a49be219866ad824e204b
wyrrdfox <farelkafur@gmail.com>
2026-05-17 16:24:57 +0700

    first to commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="root">
+    <h1>WELCOME TO MY WEB PAGE! :D</h1>
+    <p>about me</p>
+    <p>portfolio</p>
+    <p>contact me</p>
+</div>
+
+<style>
+    .root{
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
+
